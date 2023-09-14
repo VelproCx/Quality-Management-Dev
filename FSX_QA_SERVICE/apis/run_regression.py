b14fcf39127b8c8959952492ccbf4ecedf915510
huangmiaolan <huangmiaolan@farsightedyu.com>
2023-09-14 15:29:53 +0900

    optimized regression run code, return time

--- FSX_QA_SERVICE/apis/run_regression.py
+++ FSX_QA_SERVICE/apis/run_regression.py
@@ -1,31 +1,59 @@
+import json
 import os
 
-from flask import Flask, jsonify, send_file
+from flask import Flask, jsonify, send_file, Response
 import subprocess
+import time
+from datetime import datetime, timedelta
+
+
+# 使用 str() 函数将 timedelta 对象转换为可序列化的形式
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, timedelta):
+            return str(obj)
+        return super().default(obj)
+
 
 app = Flask(__name__)
+app.json_encoder = CustomJSONEncoder
 
 
+# 运行 regression
 @app.route('/api/run_regression', methods=['GET'])
 def run_regression():
     # 定义文件路径和文件名
-    file_path = '../edp_regression_test'
-    file_name = 'edp_regression_application.py'
+    file_path = '../edp_regression_test/edp_regression_application.py'
+
+    start_time = datetime.now()  # 记录开始时间
 
     try:
-        # 构建命令和参数列表
-        command = ['python3', file_path + '/' + file_name]
+        result = subprocess.run(['python3', file_path], capture_output=True, text=True, timeout=600)
+        output = result.stdout.strip() if result.stdout else result.stderr.strip()
+        success = True
 
-        # 执行命令并获取输出
-        output = subprocess.check_output(command, universal_newlines=True)
-
-        # 返回执行结果
-        return jsonify({'output': output})
     except subprocess.CalledProcessError as e:
-        # 处理命令执行出错的情况
-        return jsonify({'error': str(e)}), 500
+        output = e.stderr.strip()
+        success = False
+
+    except subprocess.TimeoutExpired:
+        output = "Execution time out"
+        success = False
+
+    end_time = datetime.now()  # 记录结束时间
+    execution_time = end_time - start_time  # 计算执行时间
+
+    response = {
+        'success': success,
+        'output': output,
+        'execution_time': str(execution_time),
+        'end_time': end_time.strftime("%Y-%m-%d %H:%M:%S")
+    }
+    json_response = json.dumps(response)
+    return Response(json_response, mimetype='application/json')
 
 
+# 下载 edp_report.log
 @app.route('/api/download_logs', methods=['GET'])
 def download_log_file():
     file_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + \
@@ -33,6 +61,7 @@ def download_log_file():
     return send_file(file_path, as_attachment=True)
 
 
+# 下载 edp_report.xlsx
 @app.route('/api/download_reports', methods=['GET'])
 def download_report_file():
     file_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + \
