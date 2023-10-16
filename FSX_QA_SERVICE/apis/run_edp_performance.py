f437b88b444024db4f4b9bc217335693fae44062
zhenghuaimao <zhenghuaimao@farsightedyu.com>
2023-10-16 19:46:50 +0900

    更新压力测试接口--run_enp_performance

--- FSX_QA_SERVICE/apis/run_edp_performance.py
+++ FSX_QA_SERVICE/apis/run_edp_performance.py
@@ -10,7 +10,7 @@ import tempfile
 from flask_cors import CORS
 from datetime import datetime
 from FSX_QA_SERVICE.apis.Application import global_connection_pool
-from flask import send_file, Response, request, jsonify, Blueprint, make_response, copy_current_request_context
+from flask import send_file, Response, request, jsonify, Blueprint, make_response, stream_with_context
 app_run_edp_performance = Blueprint("run_edp_performance", __name__)
 CORS(app_run_edp_performance, supports_credentials=True)
 
@@ -41,101 +41,86 @@ def create_zip_archive(file_paths, zip_file_path):
             zipf.write(file_path, os.path.basename(file_path))
 
 
-# 压力测试等待子进程结束方法
-def wait_and_handle_subprocess(processes):
-    statuses = []
-    outputs = []
-    for process in processes:
-        process.wait()
-        status = process.returncode
-        output_stdout, output_stderr = process.communicate()
-        statuses.append(status)
-        outputs.append((output_stdout, output_stderr))
-
-    for status, output in zip(statuses, outputs):
-        output_stdout, output_stderr = output
-        print("这是output_stdout：", output_stdout)
-        # 处理输出，可以根据需要进行处理
-        print("这是output_stderr：", output_stderr)
-
-
-# 压力测试异步处理--Flask 的异步任务处理机制
-@copy_current_request_context
-def send_progress_message(task_id, creator):
-    # 在此处向前端发送"压力测试进行中"的消息
-    # 可以使用 WebSocket 或其他实时通信机制
-    # 从数据库池获取数据库连接
-    connection = global_connection_pool.connection()
-    # 创建游标
-    cursor = connection.cursor()
-
-    response = {
-
-    }
-    pass
-
-@app_run_edp_performance.route('/api/edp_performance_list/run_edp_performance', methods=['POST'])
-def run_edp_performance():
-    # 获取参数并将其转换为json格式
-    data = request.get_data()
-    if not data:
-        return jsonify({"error": "Invalid request data"}), 400
+def tst(data):
+    # 判空处理
+    if not data or data == b'':
+        yield jsonify({"error": "Invalid request data"}), 400
+        return
+        # return或者直接执行默认参数
+    # 数据转换
     datas = json.loads(data)
+
+    task_id = get_task_id()
     creator = datas["source"]
-    taskid = get_task_id()
-    run_all_shell = []
-    for param in datas['commands']:
-        # 构建shell命令
-        shell_command = param['value'] + " &\n" + "sleep 1\n"
-        run_all_shell.append(shell_command)
+
+    # 创建一个空数组用于存放shell命令
+    commands = []
+    # 循环从请求体中将shell命令读取出来
+    for command in datas["commands"]:
+        shell = command["value"] + " &\n" + "sleep 1\n"
+        commands.append(shell)
     try:
-        processes = []
-        command = ''.join(run_all_shell)
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        processes.append(process)
-        # 获取子进程的执行状态码和输出
+        # 格式化数组中的shell命令
+        shell_commands = ''.join(commands)
+        # 将shell_commands用Popen方法执行
+        process = subprocess.Popen(shell_commands, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # 读取process子进程的执行结果，poll方法执行时，如果子进程未结束，则返回None
+        result = process.poll()
+        if result is None:
+            result = "progressing"
+            # 先将压力测试任务创建成功返回给接口，然后继续等待压力测试脚本执行结果
+            insert_performance_record(task_id, creator, result)
+            response = {
+                'creator': creator,
+                'taskId': task_id,
+                'status': result,
+                'type': 1
+            }
+            yield 'data: {}\n\n'.format(json.dumps(response))
+
+        # 创建两个空数组用来存放shell命令的执行结果（status）还有执行输出（outputs）
         statuses = []
         outputs = []
-        status_code = process.poll()
-        if status_code is None:
-            result = "progressing"
-            # 向前端发送压力测试进行中的msg，并让接口后台运行，等待子进程结束
-            pass
-        for process in processes:
-            process.wait()
-            result = "completed"
-            status = process.returncode
-            output_stdout, output_stderr = process.communicate()
-            statuses.append(status)
-            outputs.append((output_stdout, output_stderr))
-
-        for status, output in zip(statuses, outputs):
-            output_stdout, output_stderr = output
-            print("这是output_stdout：", output_stdout)
-            # 处理输出，可以根据需要进行处理
-            print("这是output_stderr：", output_stderr)
+        process.wait()
+        result = "completed"
+        status = process.returncode
+        outputs_stdout, outputs_stderr = process.communicate()
+        statuses.append(status)
+        outputs.append((outputs_stdout, outputs_stderr))
 
     except subprocess.CalledProcessError as e:
         output = e.stderr.strip()
         result = "error"
-        print(output, result)
+        response = {
+            "result": result,
+            "error": str(e),
+            "output": output
+        }
+        return response
+    #     修改成return之后没有调试
 
     except subprocess.TimeoutExpired:
         output = "Execution time out"
         result = "error"
-        print(output, result)
-
+        response = {
+            "result": result,
+            "error": "TimeoutExpired",
+            "output": output
+        }
+        return response
+        #     修改成return之后没有调试
+    update_performance_record(task_id, result)
     response = {
         'creator': creator,
-        'taskId': taskid,
+        'taskId': task_id,
         'status': result,
-        # 'output': output,
-        # 'start_time': start_time.strftime("%Y-%m-%d %H:%M:%S"),
-        # 'execution_time': str(execution_time),
-        # 'end_time': end_time.strftime("%Y-%m-%d %H:%M:%S"),
         'type': 1
     }
+    yield 'data: {}\n\n'.format(json.dumps(response))
 
+
+# 向PerformanceRecord插入数据
+def insert_performance_record(task_id, creator, status):
     # 从数据库池获取数据库连接
     connection = global_connection_pool.connection()
     # 创建游标
@@ -144,32 +129,145 @@ def run_edp_performance():
     # 构建SQL语句
     sql = "INSERT INTO `PerformanceRecord` (`taskId`, `type`, `createUser`, `status`) " \
           "VALUES (%s, %s, %s, %s)"
-    values = (taskid, response['type'], creator, response["status"])
+    values = (task_id, 1, creator, status)
 
     try:
         cursor.execute(sql, values)
         connection.commit()
     except Exception as e:
-        print("Error while inserting into the database:", e), 500
-
+        print("Error while inserting into the database:", e)
+        raise
     finally:
         cursor.close()
         connection.close()
-    json_response = json.dumps(response)
+    print("执行insert_performance_record（）成功")
 
-    return Response(json_response, mimetype='application/json'), 200
+
+def update_performance_record(task_id, status):
+    # 从数据库池获取数据库连接
+    connection = global_connection_pool.connection()
+    # 创建游标
+    cursor = connection.cursor()
+    # 构建sql语句
+    sql = "UPDATE `PerformanceRecord` SET `status`  = %s WHERE `taskId` = %s"
+    values = (status, task_id)
+    try:
+        cursor.execute(sql, values)
+        connection.commit()
+    except Exception as e:
+        print("Error while updating the database:", e)
+        raise
+    finally:
+        cursor.close()
+        connection.close()
+    print("执行update_performance_record（）成功")
+    response = {
+        'taskId': task_id,
+        'status': status,
+        'type': 1
+    }
+    return response
+
+
+async def run_script(task_id, creator, commands):
+    # 创建一个空数组用于存放shell命令
+    shell_commands = []
+    # 循环从请求体中将shell命令读取出来
+    for command in commands:
+        shell = command["value"] + " &\n" + "sleep 1\n"
+        shell_commands.append(shell)
+    try:
+        # 格式化数组中的shell命令
+        shell_commands = ''.join(shell_commands)
+        # 将shell_commands用Popen方法执行
+        process = subprocess.Popen(shell_commands, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process.wait()
+        result = "completed"
+        statuses = []
+        outputs = []
+        status = process.returncode
+        outputs_stdout, outputs_stderr = process.communicate()
+
+        statuses.append(status)
+        outputs.append((outputs_stdout, outputs_stderr))
+
+        # 更新PerformanceRecord记录状态
+        update_performance_record(task_id, result)
+
+        response = {
+            'creator': creator,
+            'taskId': task_id,
+            'status': result,
+            'type': 1
+        }
+        return response, 200
+
+    except subprocess.CalledProcessError as e:
+        output = e.stderr.strip()
+        result = "error"
+        response = {
+            "result": result,
+            "error": str(e),
+            "output": output
+        }
+        return response, 500
+
+    except subprocess.TimeoutExpired:
+        output = "Execution time out"
+        result = "error"
+        response = {
+            "result": result,
+            "error": "TimeoutExpired",
+            "output": output
+        }
+        return response, 500
+
+
+@app_run_edp_performance.route('/api/edp_performance_list/run_edp_performance', methods=['POST'])
+def run_edp_performance():
+    # 从请求体中获取数据
+    data = request.get_data()
+    # 判空处理
+    if not data or data == b'':
+        return jsonify({"error": "Invalid request data"}), 400
+        # 数据转换
+    # datas = json.loads(data)
+    # print("获取数据成功")
+    #
+    # task_id = get_task_id()
+    # creator = datas["source"]
+    # commands = datas["commands"]
+    # print("准备执行数据插入")
+    # # 先将压力测试任务创建成功返回给接口，然后继续等待压力测试脚本执行结果
+    # insert_performance_record(task_id, creator, "progressing")
+    # print("准备开始执行异步函数")
+    # # 使用asyncio创建事件循环并执行异步脚本函数
+    # loop = asyncio.new_event_loop()
+    # asyncio.set_event_loop(loop)
+    # result = loop.run_until_complete(run_script(task_id, creator, commands))
+    # loop.close()
+    # return jsonify(result)
+        # 设置响应头，指定内容类型为text/event-stream
+    headers = {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    }
+
+    # 将生成器转换为响应对象，并传递响应头
+    return Response(stream_with_context(tst(data)), headers=headers)
 
 
 @app_run_edp_performance.route('/api/edp_performance_list/download_performance_logs', methods=['GET'])
 def download_performance_log_file():
-    date = request.args.to_dict()
+    data = request.args.to_dict()
     # 判断传参是否为空
-    if date is None or date == '':
+    if data is None or data == '':
         return jsonify({"Error": "Invalid file path"}), 400
 
     # 创建一个空数组用于存放所有日志的路径
     file_paths = []
-    accounts = date["account"]
+    accounts = data["account"]
     # 循环从参数中读取account，并拼接日志地址
     for account in accounts:
         log_path = "edp_fix_client/initiator/edp_performance_test/report/{}_Report.log".format(account)
@@ -212,9 +310,9 @@ def view_edp_performance_case():
         # 读取json文件
         with open(case_file_path, "r") as file:
             file_content = file.read()
-            date = json.loads(file_content)
+            data = json.loads(file_content)
         # 获取testCase列表
-        test_cases = date["testCase"]
+        test_cases = data["testCase"]
         # 统计Symbol数量
         symbol_count = len([test_case["Symbol"] for test_case in test_cases])
         response = {
@@ -255,10 +353,11 @@ def edp_performance_list():
         sql = sql + ' ORDER BY `start_date` DESC'
         try:
             # 统计数据总数
-            cursor.execute("SELECT COUNT(*) as total_count FROM `qa_admin`.PerformanceRecord WHERE `type` = 1" + sql)
+            cursor.execute('SELECT COUNT(*) as total_count FROM `qa_admin`.PerformanceRecord '
+                           'WHERE `type` = 1 {}'.format(sql))
             total_count = cursor.fetchone()["total_count"]
             # 查询数据
-            cursor.execute("SELECT * FROM `qa_admin`.PerformanceRecord WHERE `type` = 1" + sql)
+            cursor.execute("SELECT * FROM `qa_admin`.PerformanceRecord WHERE `type` = 1 {}".format(sql))
             search_result = cursor.fetchall()
             data = [process_row(row) for row in search_result]
             response = {
